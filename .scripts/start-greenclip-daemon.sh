0117c1d52fa2bae8e1b03adc9cad22d1fd6e357d
2goodAP <45424904+2goodAP@users.noreply.github.com>
2019-11-26 08:02:02 +0545

    Change i3 config to start greenclip daemon

--- .scripts/start-greenclip-daemon.sh
+++ .scripts/start-greenclip-daemon.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Only run the greenclip daemon once
-if ps -eo 'tty,comm' | grep -q 'greenclip'; then
+if ps -eo 'tty,comm' | grep -q '^\?.*greenclip'; then
     exit 0
 else
     exec greenclip daemon &
