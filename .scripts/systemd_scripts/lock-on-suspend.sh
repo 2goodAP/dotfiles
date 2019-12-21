c9b38268c0c264182c5f9e7e70fc63dcc26eac3b
2goodAP <45424904+2goodAP@users.noreply.github.com>
2019-12-21 18:04:21 +0545

    Change systemlock on suspend unit and script files

--- .scripts/systemd_scripts/lock-on-suspend.sh
+++ .scripts/systemd_scripts/lock-on-suspend.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Script for initiating lock program
 export DISPLAY=:0
-export XDG_RUNTIME_DIR=/run/user/$UID
 
 if ls /run/user/$UID | grep -q 'wayland'; then
+    export XDG_RUNTIME_DIR=/run/user/$UID
     export WAYLAND_DISPLAY=wayland-0
     exec /usr/bin/swaylock
 else
