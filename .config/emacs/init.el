12a2d0bc29a6dd2e3128d721b2847c71297519e4
2goodAP <45424904+2goodAP@users.noreply.github.com>
2022-02-28 09:52:20 +0545

    Change fonts in emacs

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -3,15 +3,23 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(warning-suppress-types '((comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 120 :width normal :family "Fira Code"))))
+ '(bold ((t (:weight bold :height 1.0 :family "Fira Code"))))
+ '(bold-italic ((t (:slant italic :weight bold :family "Victor Mono"))))
+ '(italic ((t (:slant italic :weight medium :family "Victor Mono")))))
 
-;; Customize straight.el package manager settings
+
+;; Change font related attributes
+(set-face-attribute 'default t :family "Fira Code" :height 120)
+
+
+;; Custom settings for straight.el package manager
 (setq straight-check-for-modifications '(check-on-save find-when-checking))
 
 ;; Bootstrap straight.el package manager
