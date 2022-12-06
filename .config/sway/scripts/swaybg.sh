ee8bc5b42c72ee700bdbb6dae7990a44ef75c274
2goodAP <45424904+2goodAP@users.noreply.github.com>
2022-12-06 07:45:45 +0545

    Change swawm swaylock and swaybg wallpaper detection

--- .config/sway/scripts/swaybg.sh
+++ .config/sway/scripts/swaybg.sh
@@ -2,8 +2,7 @@
 # A very simple script to set desktop background per user on the fly.
 #
 
-USER="$(whoami)"
-LOG_FILE="/tmp/swaybg-$USER.log"
+LOG_FILE="/tmp/swaybg-${USER}.log"
 
 # `swaybg` Parameters
 OUTPUT='*'
@@ -11,11 +10,13 @@ MODE='fill'
 FALLBACK='#ffffff'
 
 # Background Image
-BACKGROUND="$HOME/Pictures/Desktop_Wallpaper.jpg"
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+USER="$(whoami)"
+BACKGROUND="${SCRIPT_DIR}/../images/${USER}_desktop.jpg"
 
 # Terminate already running swaybg instances.
 killall -q swaybg
 
 # Apply the background to the specified output.
-swaybg --output=$OUTPUT --image=$BACKGROUND --mode=$MODE --color=$FALLBACK \
-    2>&1 | tee -a $LOG_FILE & disown
+swaybg --output=${OUTPUT} --image=${BACKGROUND} --mode=${MODE} \
+    --color=${FALLBACK} 2>&1 | tee -a ${LOG_FILE} & disown
--- .config/sway/scripts/swaylock.sh
+++ .config/sway/scripts/swaylock.sh
@@ -2,11 +2,12 @@
 # A very simple script to launch swaylock with custom settings.
 #
 
-USER="$(whoami)"
-LOG_FILE="/tmp/swaylock-$USER.log"
+LOG_FILE="/tmp/swaylock-${USER}.log"
 
 # Background Image
-BACKGROUND="$HOME/Pictures/Lockscreen_Wallpaper.jpg"
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+USER="$(whoami)"
+BACKGROUND="${SCRIPT_DIR}/../images/${USER}_lockscreen.jpg"
 
 # Colors
 BLUE=1c71d8ff
@@ -18,21 +19,19 @@ TRANSPARENT=24242400
 YELLOW=e5a50aff
 
 # Swaylock Command
-swaylock --daemonize --ignore-empty-password --image=$BACKGROUND \
-    --scaling=fill --effect-blur=10x1 --effect-vignette=0.75:0.75 \
-    --indicator --indicator-radius=80 --indicator-thickness=15 \
+swaylock --daemonize --ignore-empty-password --image ${BACKGROUND} \
+    --scaling fill --indicator-radius 80 --indicator-thickness 15 \
     --indicator-caps-lock --disable-caps-lock-text \
-    --clock --timestr='%I:%M %p' --datestr='%a, %b %d, %Y' \
-    --key-hl-color=$GREEN --bs-hl-color=$RED --separator-color=$PRIMARY \
-    --inside-color=$TRANSPARENT --inside-clear-color=$TRANSPARENT \
-    --inside-caps-lock-color=$TRANSPARENT --inside-ver-color=$TRANSPARENT \
-    --inside-wrong-color=$TRANSPARENT \
-    --line-color=$PRIMARY --line-clear-color=$PRIMARY \
-    --line-caps-lock-color=$PRIMARY --line-ver-color=$PRIMARY \
-    --line-wrong-color=$PRIMARY \
-    --ring-color=$PRIMARY --ring-clear-color=$SECONDARY \
-    --ring-caps-lock-color=$YELLOW --ring-ver-color=$BLUE \
-    --ring-wrong-color=$RED \
-    --text-color=$PRIMARY --text-clear-color=$TRANSPARENT \
-    --text-caps-lock-color=$PRIMARY --text-ver-color=$TRANSPARENT \
-    --text-wrong-color=$TRANSPARENT 2>&1 | tee -a $LOG_FILE & disown
+    --key-hl-color ${GREEN} --bs-hl-color ${RED} --separator-color ${PRIMARY} \
+    --inside-color ${TRANSPARENT} --inside-clear-color ${TRANSPARENT} \
+    --inside-caps-lock-color ${TRANSPARENT} --inside-ver-color ${TRANSPARENT} \
+    --inside-wrong-color ${TRANSPARENT} \
+    --line-color ${PRIMARY} --line-clear-color ${PRIMARY} \
+    --line-caps-lock-color ${PRIMARY} --line-ver-color ${PRIMARY} \
+    --line-wrong-color ${PRIMARY} \
+    --ring-color ${PRIMARY} --ring-clear-color ${SECONDARY} \
+    --ring-caps-lock-color ${YELLOW} --ring-ver-color ${BLUE} \
+    --ring-wrong-color ${RED} \
+    --text-color ${PRIMARY} --text-clear-color ${TRANSPARENT} \
+    --text-caps-lock-color ${PRIMARY} --text-ver-color ${TRANSPARENT} \
+    --text-wrong-color ${TRANSPARENT} 2>&1 | tee -a ${LOG_FILE} & disown
